d19b6f9f1bea9c05a2feece5dacc11bec52a5283
Filip Sobczak <filsob@gmail.com>
2023-06-11 17:10:16 +0200

    fix ref

--- app/api/balances/[id]/route.ts
+++ app/api/balances/[id]/route.ts
@@ -6,14 +6,14 @@ const { collection } = responseMock.data;
 export async function GET(
   request: NextRequest,
   {
-    params,
+    params: { id },
   }: {
     params: { id: string };
   }
 ) {
   const found = collection.find((item) => item.balanceId === id);
   if (!found) {
-    throw new Error("return 404");
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
   }
 
   return NextResponse.json(responseMock);
